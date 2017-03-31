869f89bfc970a883925acf86416e4086b4cf3542
sim.yu <176912231@163.com>
2017-03-31 22:58:41 +0800

    add nginx.conf

--- fabfile.py
+++ fabfile.py
@@ -7,10 +7,32 @@ from fabric.api import roles
 
 
 env.roledefs = {
-    'remote_server': ['root@45.77.18.107']
+    'remote': ['root@45.77.18.107']
 }
 
 
-@roles('remote_server')
+@roles('remote')
+def init():
+    '''环境初始化'''
+    pass
+
+
+@roles('remote')
+def deploy():
+    '''部署'''
+    pass
+
+
+@roles('remote')
+def close():
+    '''关闭'''
+    pass
+
+
+@roles('remote')
 def restart():
-    run('')
+    '''重启'''
+    # reload nginx
+    run('nginx -s /opt/dinglive/nginx.conf')
+    run('systemctl restart mysqld')
+    run('supervisord -c /opt/dinglive/supervisord.conf')
