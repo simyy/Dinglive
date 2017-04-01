990629b8468db2a70ebb76e1073fbac3414c9a47
sim.yu <176912231@163.com>
2017-04-01 23:54:00 +0800

    update fabric

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-# path
-logPath="/opt/logs/dinglive"
-if [ ! -d "$logPath" ]; then
-    mkdir "$logPath"
-fi
-
-# supervidord
-supervisord -c /opt/dinglive/supervisord.conf
--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,10 @@ from fabric.api import cd
 from fabric.api import env
 from fabric.api import run
 from fabric.api import roles
+from fabric.colors import green
+from fabric.colors import yellow
+
+import time
 
 
 env.roledefs = {
@@ -15,13 +19,26 @@ env.roledefs = {
 @roles('remote')
 def deploy(branch):
     '''部署'''
+    print(green('开始部署'))
     # 创建运行空间, 更新代码
+    print(yellow('1.创建空间'))
     run('mkdir -p /opt/logs/dinglive')
+    print(yellow('2.同步代码'))
     with cd('/opt'):
         run('git clone https://github.com/simyy/dinglive.git')
         run('git checkout %s' % branch)
-    # 安装nginx、mysql
-    # 安装python依赖环境
+    # 启动nginx
+    print(yellow('3.启动nginx'))
+    run('nginx -s stop')
+    time.sleep(2)
+    run('nginx -c /opt/dinglive/nginx.conf')
+    # 启动mysql
+    print(yellow('4.启动mysql'))
+    run('')
+    # 启动supervisor
+    print(yellow('5.启动supervisord'))
+    run('supervisord -c /opt/dinglive/supervisord.conf')
+    print(green('完成部署'))
 
 
 @roles('remote')
